d1c84fa5904e0ea40ad98668f4199aa26dc55fd9
Banto Balazs <balazs.banto@agroninja.com>
2021-11-14 15:17:10 +0100

    Opencv live preview modification is working

--- app/src/main/java/com/blade/testopencv/MainActivity.kt
+++ app/src/main/java/com/blade/testopencv/MainActivity.kt
@@ -2,10 +2,19 @@ package com.blade.testopencv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (OpenCVLoader.initDebug()) {
+            Log.d("MyShit", "is working")
+        } else {
+            Log.d("MyShit", "is NOT working")
+        }
     }
 }
